75baeabbef0c4780240912c3438335438628415f
Kristian Cenow <40594589+kcenow@users.noreply.github.com>
2021-09-20 21:31:21 +0300

    Delete JavaSript/Fundamentals/More directory

--- JavaSript/Fundamentals/More/04. Reverse String.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-    let strArr = str.split('');
-    let strReversed = strArr.reverse();
-    strReversed = strReversed.join('');
-    console.log(strReversed);
-}
-
-reverseString('Hello');
